674cd0a770a5f425326498f0ae7aa3d6a1998e42
morrisaiden <mwaimorris1997@gmail.com>
2024-02-29 09:45:38 -0800

    Balance factor

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+/**
+ * is_full_recursive - Determines whether a binary tree is full recursively.
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Return: 1 if the binary tree is full, 0 otherwise.
+ */
+
+int is_full_recursive(const binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		if ((tree->left != NULL && tree->right == NULL) ||
+		    (tree->left == NULL && tree->right != NULL) ||
+		    is_full_recursive(tree->left) == 0 ||
+		    is_full_recursive(tree->right) == 0)
+			return (0);
+
+	}
+	return (1);
+}
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	return (is_full_recursive(tree));
+}
